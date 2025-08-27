0f5df5d56d09060f26a880414f8dae822103e5d5
Your Nameclear <you@example.com>
2025-08-27 21:10:43 +0700

    first commit

---
+++ resources/views/aboutme.blade.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="id">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ERIK WAHYU SAPUTRA</title>  
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">   
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css">
+</head>
+<body>
+
+    <div class="profile-card">
+        <img src="{{ asset('images/ig.jpg') }}" alt="Foto Profil" class="profile-image">
+
+        <h1>About Me</h1>
+
+        <p class="bio">
+           Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti nostrum mollitia et fugit adipisci voluptatem quae deleniti cumque ex nobis aspernatur ipsum maxime, est praesentium magni doloremque id omnis at.
+        </p>
+        <div class="social-links">
+            <a href="https://www.facebook.com/erikwahyu.saputra.547" target="_blank"><i class="fab fa-facebook-f"></i></a>
+            <a href="https://twitter.com" target="_blank"><i class="fab fa-twitter"></i></a>
+            <a href="https://www.instagram.com/erikwahyusaputraa/" target="_blank"><i class="fab fa-instagram"></i></a>
+        </div>
+
+        <div class="action-buttons">
+            <a href="mailto:hi@freedesigner.com" class="btn btn-primary">saputraerik042@gmail.com</a>
+        </div>
+    </div>
+
+</body>
+</html>
