9085f8f081c0cdb63b0e41e20b3cebdeb3888d3e
Your Nameclear <you@example.com>
2025-08-28 16:54:44 +0700

    adding background,card,and optimization

--- resources/views/aboutme.blade.php
+++ resources/views/aboutme.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>ERIK WAHYU SAPUTRA</title>  
+    <title>Erik Wahyu Saputra</title>  
     <link rel="stylesheet" href="{{ asset('css/style.css') }}">   
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css">
 </head>
@@ -12,15 +12,16 @@
     <div class="profile-card">
         <img src="{{ asset('images/ig.jpg') }}" alt="Foto Profil" class="profile-image">
 
-        <h1>About Me</h1>
+        <h1>Tentang Saya</h1>
 
         <p class="bio">
-           Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti nostrum mollitia et fugit adipisci voluptatem quae deleniti cumque ex nobis aspernatur ipsum maxime, est praesentium magni doloremque id omnis at.
+            "Selalu penasaran tentang dunia di sekitar saya. Suka mengulik hal-hal teknis, tapi juga menikmati hal-hal sederhana dalam hidup. Mencari jawaban untuk setiap pertanyaan, besar atau kecil."
         </p>
         <div class="social-links">
+            <a href="https://wa.me/6285739188906" target="_blank"><i class="fab fa-whatsapp"></i></a>
             <a href="https://www.facebook.com/erikwahyu.saputra.547" target="_blank"><i class="fab fa-facebook-f"></i></a>
-            <a href="https://twitter.com" target="_blank"><i class="fab fa-twitter"></i></a>
             <a href="https://www.instagram.com/erikwahyusaputraa/" target="_blank"><i class="fab fa-instagram"></i></a>
+            <a href="https://github.com/XxMeguminxX" target="_blank"><i class="fab fa-github"></i></a>
         </div>
 
         <div class="action-buttons">
